cdb6eccefa8abaecee5af23570369068681723f3
anshucodes404 <anshukr95086@gmail.com>
2026-01-10 19:11:07 +0530

    some fixes

--- backend/src/routes/animal.route.ts
+++ backend/src/routes/animal.route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { attendance, getAttendanceRecords } from '../controllers/animal.controller';
+import { attendance, getHealthRecordsOftheAnimalInitiatedByVet } from '../controllers/animal.controller';
 import { upload } from '../middlewares/multer';
 import { verifyJWT } from '../middlewares/verifyJwt';
 
 const router = Router();
 
 router.post('/attendance', verifyJWT, upload.single('image'), attendance);
-router.get('/:animalId/attendance', verifyJWT, getAttendanceRecords);
+router.get('/:tagId/health-records', verifyJWT, getHealthRecordsOftheAnimalInitiatedByVet);
 
 export default router;
